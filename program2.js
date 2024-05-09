88e92a4f80f44d0844f738f0c2063b4e4a4dcd1c
SHUBHAM AWASYA <63654949+SubhamAwasya@users.noreply.github.com>
2024-05-09 12:35:33 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -2,7 +2,7 @@ const decodeTheRing = function (s, p) {
   // write your code here
 
   console.log("==================" + s.length);
-  console.log("==================" + s.length);
+  console.log("==================" + .length);
 };
 
 module.exports = decodeTheRing;
