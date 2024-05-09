756c07d4546c017c8247b68ef73fbdace8225c25
SHUBHAM AWASYA <63654949+SubhamAwasya@users.noreply.github.com>
2024-05-09 12:52:36 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -11,7 +11,7 @@ const decodeTheRing = function (s, p) {
 
   let result = true;
 
-  for (let i = 0; i < array.length; i++) {
+  for (let i = 0; i < s.length; i++) {
     const element = array[i];
     
   }
