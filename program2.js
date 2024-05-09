1ede58fe902ed751d8e153e5f37bf65175148eed
SHUBHAM AWASYA <63654949+SubhamAwasya@users.noreply.github.com>
2024-05-09 13:07:32 +0530

    Auto commit on save

--- program2.js
+++ program2.js
@@ -15,7 +15,7 @@ const decodeTheRing = function (s, p) {
   if (p.includes("*")) {
     let str = p.split("*");
     const isPresent = str.lastIndexOf(str[0]) === str.length - str[0];
-    console.log(str);
+    console.log("======================" + isPresent);
   }
 
   if (p.includes("?")) {
